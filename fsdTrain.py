95a9211eeb512a5e8d59a7b37f221a1a53fcb932
hephzibah-delfia123 <80846497+hephzibah-delfia123@users.noreply.github.com>
2021-06-19 11:07:45 +0530

    Update fsdTrain.py

--- fsdTrain.py
+++ fsdTrain.py
@@ -21,27 +21,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-
-
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
+# initialize the initial learning rate, number of epochs to train for, and batch size
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
-
-
-
-
-
-DIRECTORY = r"D:\Machine Learning\DATASETS\PROJECTS\Fire Smoke Detector\FIRE-SMOKE-DATASET"
+DIRECTORY = r"D:\Machine Learning\DATASETS\PROJECTS\Fire Smoke Detector\Fire Dataset"
 CATEGORIES = ["FIRE", "NO_FIRE"]
 
-
-
-
-
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
+# grab the list of images in our dataset directory, then initialize the list of data (i.e., images) and class images
 print("[INFO] loading images...")
 
 data = []
@@ -58,9 +45,6 @@ for category in CATEGORIES:
         data.append(image)
         labels.append(category)
 
-
-
-
 # perform one-hot encoding on the labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
@@ -72,10 +56,6 @@ labels = np.array(labels)
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
     test_size=0.20, stratify=labels, random_state=42)
 
-
-
-
-
 # construct the training image generator for data augmentation
 aug = ImageDataGenerator(
     rotation_range=20,
@@ -86,21 +66,11 @@ aug = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode="nearest")
 
-
-
-
-
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+# load the MobileNetV2 network, ensuring the head FC layer sets are left off
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
     input_tensor=Input(shape=(224, 224, 3)))
 
-
-
-
-
-# construct the head of the model that will be placed on top of the
-# the base model
+# construct the head of the model that will be placed on top of the the base model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -108,27 +78,18 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# place the head FC model on top of the base model (this will become the actual model we will train)
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-
-
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+# loop over all layers in the base model and freeze them so they will *not* be updated during the first training process
 for layer in baseModel.layers:
     layer.trainable = False
-
 
 # compile our model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
     metrics=["accuracy"])
-
-
-
 
 # train the head of the network
 print("[INFO] training head...")
@@ -139,18 +100,12 @@ H = model.fit(
     validation_steps=len(testX) // BS,
     epochs=EPOCHS)
 
-
-
-
-
 # make predictions on the testing set
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+# for each image in the testing set we need to find the index of the label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
-
 
 # show a nicely formatted classification report
 print(classification_report(testY.argmax(axis=1), predIdxs,
@@ -158,7 +113,7 @@ print(classification_report(testY.argmax(axis=1), predIdxs,
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
-model.save("fire_smoke", save_format="h5")
+model.save("fire", save_format="h5")
 
 # plot the training loss and accuracy
 N = EPOCHS
